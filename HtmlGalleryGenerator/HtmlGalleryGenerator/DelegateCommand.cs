1271e3cea7ab9db252aed887cd544f712c2d09a6
nAXAH1@gmail.com <nAXAH1@gmail.com@b5653406-da8a-92a7-4211-0244fa0040e2>
2013-09-17 09:49:58 +0000

    Initial

---
+++ HtmlGalleryGenerator/HtmlGalleryGenerator/DelegateCommand.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace HtmlGalleryGenerator
+{
+    public class DelegateCommand : DelegateCommand<object>
+    {
+        #region public constructors
+
+        public DelegateCommand(Action execute)
+            : base(execute != null ? x => execute() : (Action<object>) null)
+        {
+        }
+
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+            : base(execute != null ? x => execute() : (Action<object>) null,
+                canExecute != null ? x => canExecute() : (Predicate<object>) null)
+        {
+        }
+
+        #endregion
+    }
+
+    public class DelegateCommand<T> : ICommand
+    {
+        #region public constructors
+
+        public DelegateCommand(Action<T> execute)
+            : this(execute, x => true)
+        {
+        }
+
+        public DelegateCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (canExecute == null) throw new ArgumentNullException("canExecute");
+            if (execute == null) throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #endregion
+
+        #region public methods
+
+        public void Execute(object parameter = null)
+        {
+            _execute((T) parameter);
+        }
+
+        public bool CanExecute(object parameter = null)
+        {
+            return _canExecute((T) parameter);
+        }
+
+        public virtual void OnCanExecuteChanged()
+        {
+            EventHandler handler = CanExecuteChanged;
+            if (handler != null) handler(this, EventArgs.Empty);
+        }
+
+        #endregion
+
+        #region public methods
+
+        public event EventHandler CanExecuteChanged;
+
+        #endregion
+
+        #region private fields
+
+        private readonly Action<T> _execute;
+        private readonly Predicate<T> _canExecute;
+
+        #endregion
+    }
+}
---
+++ HtmlGalleryGenerator/HtmlGalleryGenerator/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace HtmlGalleryGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        #region public constructors
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
